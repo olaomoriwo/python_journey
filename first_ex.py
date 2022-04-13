1b6ffbceb2fa1f57d5b054d1348890e1ee33aa46
Olasehinde Omoriwo <olasehindeomoriwo@Olasehindes-MacBook-Air.local>
2022-04-13 21:51:31 +0100

    testing my first python variable

--- first_ex.py
+++ first_ex.py
@@ -5,3 +5,10 @@ if 5 > 2:
     print("Five is greater than two!")
 if 10 > 10:
     print("Levels don change now, I'm 10 over 10")
+x = 5
+y = "Hello, World"
+print(x, y)
+x = 10
+y = "This is a cold sobo"
+print(x)
+print(y)
