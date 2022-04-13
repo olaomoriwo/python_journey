8f827942ddbd5eb12e3fd2bd08e6bbacc78f9100
Olasehinde Omoriwo <olasehindeomoriwo@Olasehindes-MacBook-Air.local>
2022-04-13 19:09:06 +0100

    My first line

--- first_ex.py
+++ first_ex.py
@@ -1,5 +1,1 @@
-print ('This marks the begining of my python journey')
-print("I love Python because it's interesting")
-print("Hello Worold, it is okay. happy :)")
-print("So I didn't know my path and could not codE")
-print("It is Okay and it can be better and better")
+print("Hello World")
