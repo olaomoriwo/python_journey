0bd88141ce71fa83fdce54692932c40a29202a3e
Olasehinde Omoriwo <olasehindeomoriwo@Olasehindes-MacBook-Air.local>
2022-04-13 17:39:34 +0100

    jUST testing

--- first_ex.py
+++ first_ex.py
@@ -1,3 +1,4 @@
 print ('This marks the begining of my python journey')
 print("I love Python because it's interesting")
-print("Hello Worold, it is okay")
+print("Hello Worold, it is okay. happy :)")
+print("So I didn't know my path and could not code for days")
