4f416882ad421ae8d57a023fa7ef87516e9da317
Olasehinde Omoriwo <olasehindeomoriwo@Olasehindes-MacBook-Air.local>
2022-04-13 22:27:59 +0100

    Trying comment functionality

--- first_ex.py
+++ first_ex.py
@@ -24,3 +24,18 @@ x = 5
 y = 10
 z = x + y
 print(z)
+#This is a comment
+print("Hello, World!")
+print("Hello, World!") #This is a comment
+#print("Hello, World!")
+print("Cheers, Mate!")
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World")
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
